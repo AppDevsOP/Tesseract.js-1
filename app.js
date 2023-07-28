5f82514b64b842278d91fd13db0bd6ad7972f956
AppDevsOP <testmesasoporte@gmail.com>
2023-07-28 15:08:03 -0500

    API v0.1

--- app.js
+++ app.js
@@ -1,33 +1,46 @@
+// app.js
+const express = require('express');
 const Tesseract = require('tesseract.js');
 
-// Ruta de la imagen que deseas procesar
-const imagePath = './Texto2.jpg';
+const app = express();
 
-// Realiza el OCR en la imagen
-Tesseract.recognize(
-  imagePath,           // Ruta de la imagen
-  'spa+eng',               // Idioma de reconocimiento (en este caso, español)
-  {
-    logger: info => console.log(info.progress) // Mostrar el progreso del OCR en la consola
-  }
-).then(({ data: { text } }) => {
-  console.log('Texto extraído:', text);
+// ... (otros middleware y rutas)
+app.use(express.json());
 
-  // Buscar las palabras clave y extraer los valores asociados
-  const keywords = ['Subtotal', 'IVA', 'Fecha', 'Total', 'Fecha vencimiento', 'Carretera' ];
+
+// Ruta para procesar la imagen y realizar el OCR
+app.post('/api/ocr', (req, res) => {
+  const imagePath = req.body.imagePath; // Asegúrate de enviar la ruta de la imagen en el cuerpo de la solicitud (en el caso de una solicitud POST)
+  const keywords = ['Subtotal', 'IVA', 'Fecha', 'Total', 'Fecha vencimiento', 'Carretera'];
   const extractedData = {};
 
-  for (const keyword of keywords) {
-    const regex = new RegExp(`${keyword}\\s+([\\d,.]+)`, 'i');
-    const match = text.match(regex);
-    if (match) {
-      extractedData[keyword] = match[1].trim(); // No se realiza el reemplazo de comas por puntos
-    } else {
-      extractedData[keyword] = 'No encontrado';
+  Tesseract.recognize(
+    imagePath,
+    'spa+eng',
+    {
+      logger: info => console.log(info.progress)
     }
-  }
+  ).then(({ data: { text } }) => {
+    for (const keyword of keywords) {
+      const regex = new RegExp(`${keyword}\\s+([\\d,.]+)`, 'i');
+      const match = text.match(regex);
+      if (match) {
+        extractedData[keyword] = match[1].trim();
+      } else {
+        extractedData[keyword] = 'No encontrado';
+      }
+    }
   
-  console.log('Datos extraídos:', extractedData);
-}).catch(error => {
-  console.error('Error:', error);
+    res.json(extractedData);
+  }).catch(error => {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Error en el procesamiento de la imagen' });
+  });
+});
+
+// ... (más rutas y configuraciones)
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
